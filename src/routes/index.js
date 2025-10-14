a03528726b688052285678c942b80f2c73507a35
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-10-14 16:42:51 +0900

    feat: API 라우팅 세팅

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import studyRouter from './study/study.js';
+import reactionRouter from './study/reaction/reaction.js';
+import habitRouter from './study/habit/habit.js';
+import habitLogRouter from './study/habit/habitLog.js';
 export const router = express.Router();
 
 // 기본 라우트
@@ -8,3 +12,10 @@ router.get('/', (req, res) => {
     timestamp: new Date().toISOString(),
   });
 });
+
+router.use('/study', studyRouter);
+router.use('/study/:studyId/reaction', reactionRouter);
+router.use('/study/:studyId/habit', habitRouter);
+router.use('/habit/:habitId/log', habitLogRouter);
+
+export default router;
