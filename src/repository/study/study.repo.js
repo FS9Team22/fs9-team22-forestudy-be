73d35b4183e9f5a037a06fabbc075b7766eb6da9
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-10-17 11:28:20 +0900

    fix: findStudyById 응답 데이터에서 비밀번호 배제

--- src/repository/study/study.repo.js
+++ src/repository/study/study.repo.js
@@ -1,7 +1,12 @@
 import { prisma } from '../../db/prisma.js';
 
 async function findStudyById(id) {
-  return await prisma.study.findUnique({ where: { id: String(id) } });
+  return await prisma.study.findUnique({
+    where: { id: String(id) },
+    omit: {
+      password: true,
+    },
+  });
 }
 
 export const studyRepo = {
