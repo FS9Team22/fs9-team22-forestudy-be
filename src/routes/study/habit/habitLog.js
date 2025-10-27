1a6728f7b7e8d67aa9ffd5ec95b01d49fdbd225c
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-10-27 11:25:32 +0900

    feat: habit Log CRD 완성

--- src/routes/study/habit/habitLog.js
+++ src/routes/study/habit/habitLog.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { habitLogRepo } from '../../../repository/study/habit/habitLog.repo.js';
+import { habitRepo } from '../../../repository/study/habit/habit.repo.js';
 import { NotFoundException } from '../../../err/notFoundException.js';
 const router = express.Router({ mergeParams: true });
 
@@ -7,11 +8,11 @@ router.get('/', async (req, res, next) => {
   try {
     const habitId = req.params.habitId;
     console.log(habitId);
-    const habits = await habitLogRepo.findHabitLogListByHabitId(habitId);
-    if (!habits) {
+    const habitLogs = await habitLogRepo.findHabitLogListByHabitId(habitId);
+    if (!habitLogs) {
       throw new NotFoundException('존재하지 않는 습관 내역입니다.');
     }
-    res.json({ success: true, data: habits });
+    res.json({ success: true, data: habitLogs });
   } catch (err) {
     next(err);
   }
@@ -21,12 +22,51 @@ router.get('/week', async (req, res, next) => {
   try {
     const habitId = req.params.habitId;
     console.log(habitId);
-    const habits =
+    const habitLogs =
       await habitLogRepo.findHabitCurrentWeekLogListByHabitId(habitId);
-    if (!habits) {
+    if (!habitLogs) {
       throw new NotFoundException('존재하지 않는 습관 내역입니다.');
     }
-    res.json({ success: true, data: habits });
+    res.json({ success: true, data: habitLogs });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/day', async (req, res, next) => {
+  try {
+    const habitId = req.params.habitId;
+    console.log(habitId);
+    const habitLogs =
+      await habitLogRepo.findHabitCurrentdayLogByHabitId(habitId);
+    if (!habitLogs) {
+      throw new NotFoundException('존재하지 않는 습관 내역입니다.');
+    }
+    res.json({ success: true, data: habitLogs });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const habitId = req.params.habitId;
+    const habitExistance = await habitRepo.findHabitById(habitId);
+    if (!habitExistance) {
+      throw new NotFoundException('존재하지 않는 습관입니다.');
+    }
+    const newHabitLog = await habitLogRepo.createHabitLogByHabitId(habitId);
+    res.json({ success: true, data: newHabitLog });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const habitLogId = req.params.id;
+    const deleteHabitLog = habitLogRepo.deleteHabitLogById(habitLogId);
+    res.json({ success: true, data: deleteHabitLog });
   } catch (err) {
     next(err);
   }
